9240d4d7e1df0df3e8bd7707f0dfa731aa9386d0
vietvanba <baviet19@gmail.com>
2023-10-01 00:09:16 +0700

    update code for search UI

--- src/components/DescriptionOfWord.js
+++ src/components/DescriptionOfWord.js
@@ -1,5 +1,48 @@
 import { React, useEffect, useState } from "react";
 import { get } from "../API";
+import { loading } from "./Loading";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
+import CarouselImage from "./CarouselImage";
+export function sense(data) {
+  return (
+    <>
+      <div>{data.sense}</div>
+      <ul className="pl-4">
+        {data.examples.map((x) => {
+          return <li className="list-disc">{x}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
+export function pronunciation(word, pro, lang) {
+  return (
+    <>
+      <div className="text-lg flex items-center pt-2">
+        <FontAwesomeIcon
+          className={
+            lang == "English"
+              ? "hover:text-blue-600 pr-2"
+              : "hover:text-red-600 pr-2"
+          }
+          icon={faVolumeUp}
+          title={word + " pronunciation " + lang}
+          onClick={() => {
+            let audio = new Audio(pro.mp3_url);
+            audio.play();
+          }}
+        />
+        <div
+          className="pb-1"
+          style={{ fontFamily: "'Lucida Grande', 'Lucida Sans Unicode'" }}
+        >
+          {pro.pronunciation}
+        </div>
+      </div>
+    </>
+  );
+}
 export function DescriptionOfWord(props) {
   const [json, SetJson] = useState();
   useEffect(() => {
@@ -13,5 +56,30 @@ export function DescriptionOfWord(props) {
         console.error(error);
       });
   }, [props.word]);
-  return <>{JSON.stringify(json)}</>;
+  return (
+    <>
+      {json ? (
+        <div className="w-full max-w-4xl mx-auto mt-4 font-sans">
+          <div className="relative">
+            <h1 className="inline-block text-4xl font-medium text-blue-600">
+              {json.word}
+            </h1>
+            <span className="italic px-4">{json.partOfSpeech}</span>
+            {pronunciation(json.word, json.uk, "English")}
+            {pronunciation(json.word, json.us, "American")}
+            <ol className="list-decimal pl-4">
+              {json.senses.map((x) => {
+                return <li>{sense(x)}</li>;
+              })}
+            </ol>
+            <CarouselImage slides={json.images} />
+          </div>
+        </div>
+      ) : (
+        <div className="w-full max-w-4xl mx-auto mt-4">
+          <div className="relative">{loading()}</div>
+        </div>
+      )}
+    </>
+  );
 }
